7741e1a4ac0e14b42175e0bedd98fab1928b83f0
JACA <carenas874@gmail.com>
2025-11-19 03:12:31 -0500

    Creacion de funciones para el retroceso de la partida

--- Monopoly.h
+++ Monopoly.h
@@ -2,20 +2,24 @@
 #define MONOPOLY_H
 
 #include <vector>
+#include <stack>
 #include "Partida.h"
 #include "User.h"
 
 struct Monopoly {
-
     // --- Estado del juego ---
     bool juegoIniciado = false;
     Partida partidaActual;
+    std::stack<Partida> partidas;  // Historial de partidas para deshacer
 
     // --- Funcionalidades del menú ---
     void mostrarMenuPrincipal(); 
     void iniciarNuevaPartida();  
     void mostrarEstadoPartida(); 
-    void jugarTurno();          
+    void jugarTurno();
+    void DevolverPartida();
+    void GuardarEstadoPartida();
+    void VerHistorial();
 };
 
 #endif
